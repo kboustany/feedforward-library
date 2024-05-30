054f2c2f23922155d278a4ba5eb257b0870336ed
Karim Boustany <156358620+kboustany@users.noreply.github.com>
2024-05-30 19:42:45 +0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -7,11 +7,9 @@ class Loss:
     """ A loss class which implements high-level features of an arbitrary 
     objective function. """
 
-
     def __init__(self):
 
         self.values = []
-
 
     def plot(self):
         """ Plots the evolution of the loss during training."""
@@ -23,21 +21,18 @@ class Loss:
         plt.grid(True)
         plt.show()
         self.values = []
-
+        
 
 class MeanSquared(Loss):
     """ A mean squared objective function used for regression tasks. """
-    
     
     def __init__(self):
 
         super().__init__()
 
-
     def value(self, y, p):
 
         return np.sum(np.square(y - p)) / y.shape[0]
-    
     
     def derivative(self, y, p):
 
@@ -47,18 +42,15 @@ class MeanSquared(Loss):
 class CrossEntropy(Loss):
     """ A cross entropy objective function used for classification tasks. """
 
-
     def __init__(self):
 
         super().__init__()
-
 
     def value(self, y, p):
 
         p = np.clip(p, 1e-9, 1 - 1e-9)
 
         return -1 * np.sum(y * np.log(p)) / y.shape[0]
-    
     
     def derivative(self, y, p):
         
