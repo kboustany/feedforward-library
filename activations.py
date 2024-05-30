f4a0022c37b0eac311900a1ecb059c85fefd80b9
Karim Boustany <156358620+kboustany@users.noreply.github.com>
2024-05-30 19:39:46 +0300

    Update activations.py

--- activations.py
+++ activations.py
@@ -4,36 +4,30 @@ import numpy as np
 class Linear:
     """ A linear (identity) activation and its derivative. """
 
-    
     def __init__(self):
 
         self.parameterized = False
-
 
     def value(self, x):
 
         return x
     
-    
     def derivative(self, x):
 
         return np.ones(x.shape)
-    
+        
     
 class ReLU:
     """ A (leaky) ReLU activation and its derivative. """
-
 
     def __init__(self, alpha=0):
 
         self.parameterized = True
         self.alpha = alpha
 
-
     def value(self, x):
 
         return np.maximum(self.alpha * x, x)
-    
     
     def derivative(self, x):
 
@@ -44,16 +38,13 @@ class ReLU:
 class Sigmoid:
     """ A sigmoid activation and its derivative. """
 
-
     def __init__(self):
 
         self.parameterized = False
 
-
     def value(self, x):
 
         return 1 / (1 + np.exp(-x))
-    
     
     def derivative(self, x):
 
@@ -63,16 +54,13 @@ class Sigmoid:
 class Tanh:
     """ A hyperbolic tangent activation and its derivative. """
 
-
     def __init__(self):
 
         self.parameterized = False
 
-
     def value(self, x):
 
         return np.tanh(x)
-    
     
     def derivative(self, x):
         
