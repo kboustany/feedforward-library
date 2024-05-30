b0a1ce13118c11328696bc59db636c5eaf100cc6
Karim Boustany <156358620+kboustany@users.noreply.github.com>
2024-05-30 19:48:48 +0300

    Update trainers.py

--- trainers.py
+++ trainers.py
@@ -6,23 +6,18 @@ class Trainer:
     """ A trainer class which trains a given neural network with a given 
     optimizer algorithm. """
 
-
     def __init__(self, network, optimizer):
 
         self.network = network
         self.optimizer = optimizer
-
         if self.network.classification == True:
             self.loss = CrossEntropy()
-
         else:
             self.loss = MeanSquared()
-
         self.training_batch = ()
         self.validation_batch = ()
         self.prepared = False
  
-
     def prepare(self, x, y, size, seed=0):
         """ Initializes the weights of the network and permutes and splits 
         training observations into training and cross-validation sets. """
@@ -31,7 +26,6 @@ class Trainer:
         x, y = permute(x, y, seed)
         self.network.x_factors = (np.mean(x[:size]), np.std(x[:size]))
         self.network.y_factors = (np.mean(y[:size]), np.std(y[:size]))
-
         if self.network.classification == True:
             self.training_batch = \
                 add_intercept(normalize(x[:size], self.network.x_factors)), \
@@ -39,7 +33,6 @@ class Trainer:
             self.validation_batch = \
                 add_intercept(normalize(x[size:], self.network.x_factors)), \
                     y[size:]
-
         else:
             self.training_batch = \
                 add_intercept(normalize(x[:size], self.network.x_factors)), \
@@ -47,9 +40,7 @@ class Trainer:
             self.validation_batch = \
                 add_intercept(normalize(x[size:], self.network.x_factors)), \
                     normalize(y[size:], self.network.y_factors)
-
         self.prepared = True
-
 
     def fit(self, epochs):
         """ Trains the neural network, implementing a learning rate decay 
@@ -59,19 +50,14 @@ class Trainer:
         x, y = self.training_batch
         p = self.network.forward(x)
         loss = self.loss.value(y, p)
-
         for epoch in range(epochs):
             self.optimizer.update(self.network, self.loss, x, y, epoch)
             p = self.network.forward(x)
-
             if self.loss.value(y, p) >= loss:
-                self.optimizer.learning.increment()
-            
+                self.optimizer.learning.increment()   
             loss = self.loss.value(y, p)
             x, y = permute(x, y, epoch)
-
         self.network.trained = True
         print(f'Training loss: {self.loss.values[-1]}.')
-
         self.loss.plot()
         self.network.clear()
